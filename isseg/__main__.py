0702552cb4bd86ceb19d9a5c556f127826d1a058
axanderssonuu <axel.andersson@it.uu.se>
2023-04-20 15:23:28 +0200

    Update __main__.py

--- isseg/__main__.py
+++ isseg/__main__.py
@@ -5,30 +5,32 @@ import pandas as pd
 
 @click.command()
 @click.argument("csv_path", type=click.Path(exists=True, dir_okay=False))
+@click.argument("csv_out", type=click.Path(exists=False, dir_okay=False))
 @click.option("--x", type=str, default="x", help="TODO")
 @click.option("--y", type=str, default="y", help="TODO")
 @click.option("--labels", type=str, default="labels", help="TODO")
-@click.option("--cell_diameter", type=click.FloatRange(min=0), default=1, help="TODO")
+@click.option("--radius", type=click.FloatRange(min=0), default=1, help="TODO")
 @click.option("--remove_background", type=bool, default=True, help="TODO")
+
 def main(
     csv_path: str,
+    csv_out: str,
     x: str,
     y: str,
     labels: str,
-    cell_diameter: float,
+    radius: float,
     remove_background: bool = True,
 ):
+    # Load the data
     data = pd.read_csv(csv_path)
 
-    # Parse into numpy land
-    xy = data[[x, y]].to_numpy()
-    labels = data[labels].to_numpy().astype("int")
-
     # Run the clustering
-    clusters, signed_edges, active_set = isseg(
-        xy, labels, cell_diameter=cell_diameter, remove_background=remove_background
+    clusters = isseg(
+        data, x=x, y=y, labels=labels, radius=radius, remove_background=remove_background
     )
 
+    data['isseg'] = clusters
+    data.to_csv(csv_out, index=False)
 
 if __name__ == "__main__":
     main()
