3fae8f8f5f0760af50d796e34c00541d704800c7
axanderssonuu <axel.andersson@it.uu.se>
2023-05-30 13:56:05 +0200

    Update __main__.py

--- isseg/__main__.py
+++ isseg/__main__.py
@@ -8,11 +8,11 @@ from isseg.isseg import isseg
 @click.command()
 @click.argument("csv_path", type=click.Path(exists=True, dir_okay=False))
 @click.argument("csv_out", type=click.Path(exists=False, dir_okay=False))
-@click.option("-x", "--x", type=str, default="x", help="TODO")
-@click.option("-y", "--y", type=str, default="y", help="TODO")
-@click.option("-l", "--label", type=str, default="label", help="TODO")
-@click.option("-r", "--radius", type=click.FloatRange(min=0), default=1, help="TODO")
-@click.option("--remove-background/--no-remove-background", default=True, help="TODO")
+@click.option("-x", "--x", type=str, default="x", help="Column in the CSV file where the x coordinate is stored for each gene.")
+@click.option("-y", "--y", type=str, default="y", help="Column in the CSV file where the y coordinate is stored for each gene.")
+@click.option("-l", "--label", type=str, default="label", help="Column in the CSV file where the label of each gene is stored.")
+@click.option("-r", "--radius", type=click.FloatRange(min=0), default=1, help="Approximate radius of a cell.")
+@click.option("--remove-background/--no-remove-background", default=True, help="Specify whether to remove background genes or not. Default is true.")
 @click.version_option(version=version)
 def main(
     csv_path: str,
